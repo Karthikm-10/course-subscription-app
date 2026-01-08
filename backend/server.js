a45454018e6fd51b201819eaa770a3883cf6824a
Karthikm-10 <karthikmadagoni10@gmail.com>
2026-01-08 12:01:52 +0530

    middleware order

--- backend/server.js
+++ backend/server.js
@@ -2,30 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import courseRoutes from "./routes/courseRoutes.js";
 import subscriptionRoutes from "./routes/subscriptionRoutes.js";
 import authMiddleware from "./middleware/authMiddleware.js";
 
-dotenv.config(); // to access env variables
+dotenv.config();
 
 connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API Running...");
-});
-
-
-app.get("/protected", authMiddleware, (req, res) => {
-  res.json({ message: "Protected route", user: req.user });
-});
 app.use(cors({
   origin: "*",
   credentials: true
 }));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+app.get("/protected", authMiddleware, (req, res) => {
+  res.json({ message: "Protected route", user: req.user });
+});
 
 app.use("/auth", authRoutes);
 app.use("/courses", courseRoutes);
@@ -34,5 +35,5 @@ app.use("/subscribe", subscriptionRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on the port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
